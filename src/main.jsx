ccb283a22d38d797dbb9f17e1f4a55748e09097d
Macedovin <macedo.vp@gmail.com>
2023-07-28 08:37:11 -0300

    🚑 🔥 Clearing unused hook

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import GlobalStyles from './styles/global';
 
 import { CustomThemeProvider } from './hooks/theme';
 import { AuthProvider } from './hooks/auth';
-import{ DishesProvider } from './hooks/dishes' 
 import { SearchDataProvider } from './hooks/searchData';
-
+import { ConfirmDialogProvider } from './hooks/confirmDialog';
+//import { CartProvider } from './hooks/cart';
+//import{ DishesProvider } from './hooks/dishes' 
 
 import { ToastContainer } from 'react-toastify';
  
@@ -16,16 +17,17 @@ import { Routes } from './routes';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CustomThemeProvider>
-      <Fragment>
-        <GlobalStyles />
-        <ToastContainer autoClose={3000}/>        <AuthProvider>
-          <DishesProvider>
-          <SearchDataProvider>
+      <GlobalStyles />
+      <ToastContainer autoClose={3000}/>        
+      <AuthProvider>
+        <ConfirmDialogProvider>
+        <SearchDataProvider>
+          {/* <CartProvider> */}
             <Routes />
-          </SearchDataProvider>
-          </DishesProvider>
-        </AuthProvider>
-      </Fragment>
+          {/* </CartProvider> */}
+        </SearchDataProvider>
+        </ConfirmDialogProvider>
+      </AuthProvider>
     </CustomThemeProvider>
   </React.StrictMode>
 )
